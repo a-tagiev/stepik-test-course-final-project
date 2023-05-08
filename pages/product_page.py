752c080d5af582decc40a5c2508414271a864615
a-tagiev <antn.tgv@gmail.com>
2023-05-08 22:49:31 +0300

    Initial commit

---
+++ pages/product_page.py
@@ -0,0 +1,29 @@
+import math
+
+from selenium.common import NoAlertPresentException
+
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        login_link = self.browser.find_element(*ProductPageLocators.BUY_BUTTON)
+        login_link.click()
+
+    def should_be_add_button(self):
+        assert self.is_element_present(*ProductPageLocators.BUY_BUTTON), "Button isn't presented"
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
